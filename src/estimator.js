59b045b29d288938290c96fab5d10bdf4808197d
yusufAzeez <yusuf.abdulazeez1912@gmail.com>
2020-04-10 18:22:34 +0100

    My new sdg project

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,50 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+	data = {
+		region: {
+			name: name,
+			avgAge: avgAge,
+			avgDailyIncomeInUSD: avgDailyIncomeInUSD,
+			avgDailyIncomePopulation: avgDailyIncomePopulation
+		},
+		periodType: "days",
+		timeToElapse: timeToElapse,
+		reportedCases: reportedCases,
+		population: population,
+		totalHospitalBeds: totalHospitalBeds
+	}
+
+	
+	return {
+		
+		impact: {
+			currentlyInfected: reportedCases * 10,
+			infectionsByRequestedTime: infectionRate(days),
+			severeCasesByRequestedTime: 0.15 * infectionsByRequestedTime,
+			hospitalBedsByRequestedTime: (0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+			casesForICUByRequestedTime: 0.05 * infectionsByRequestedTime,
+			casesForVentilatorsByRequestedTime: 0.02 * infectionsByRequestedTime,
+			dollarsInFlight: infectionsByRequestedTime x avgDailyIncomePopulation x avgDailyIncomePopulation x timeToElapse
+		},
+
+		severeImpact: {
+			currentlyInfected: reportedCases * 50,
+			infectionsByRequestedTime: infectionRate(days),
+			severeCasesByRequestedTime: 0.15 * infectionsByRequestedTime,
+			hospitalBedsByRequestedTime: (0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+			casesForICUByRequestedTime: 0.05 * infectionsByRequestedTime,
+			casesForVentilatorsByRequestedTime: 0.02 * infectionsByRequestedTime,
+			dollarsInFlight: infectionsByRequestedTime x avgDailyIncomePopulation x avgDailyIncomePopulation x timeToElapse
+		}
+
+	};
+};
+
+function infectionRate(days) {
+	let result;
+	let factor = int(days / 3);
+	let factorPower = Math.pow(2, factor);
+	result = this.currentlyInfected * factorPower;
+	return result
+}
 
 export default covid19ImpactEstimator;
